a96404c14ffae7fcc08b7ae3c5d0c8142a2f1f40
pumpkin <pumpkin_hua@qq.com>
2016-03-31 16:32:05 +0800

    Create clear.sh
    elasticsearch优化脚本

---
+++ 4 elasticsearch/clear.sh
@@ -0,0 +1,13 @@
+
+#!/bin/sh  
+      
+#  
+#always keep nearly seven days logstash indics in cluster.    
+#  
+#  
+      
+curator --host 127.0.0.1 delete indices --index .logstash- --older-than 7 --time-unit days --timestring %Y.%m.%d  // 这一行就是删除7天前的数据
+      
+curator --timeout 36000 --host 127.0.0.1 optimize --max_num_segments 1 indices --older-than 1 --time-unit days --timestring '%Y.%m.%d' --prefix logstash-    //这一行是优化当天的倒排索引文件，提高搜索效率
+
+echo "delete successed!"
